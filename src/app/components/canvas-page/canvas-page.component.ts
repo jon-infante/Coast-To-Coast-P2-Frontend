125796e58b01f4c987fe38a6388bc4f542d2ed0a
jon-infante <jonjoninfante@gmail.com>
2022-02-04 12:50:13 -0500

    google vision labels working

--- src/app/components/canvas-page/canvas-page.component.ts
+++ src/app/components/canvas-page/canvas-page.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GooglevisionService } from '../../services/googlevision.service';
+
+
+
+
 @Component({
   selector: 'app-canvas-page',
   templateUrl: './canvas-page.component.html',
   styleUrls: ['./canvas-page.component.css']
 })
 export class CanvasPageComponent implements OnInit {
+  //api key params
+  googleResponse = {}
 
-  constructor() { }
+  constructor(private googlevision: GooglevisionService) { }
+
+  ngOnInit(){
+    }
+
   //Saves the current canvas as data url image
   saveImage(){
     var canvas = document.getElementById('appCanvas') as HTMLCanvasElement;
-    var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    var image = canvas.toDataURL("image/png");
     return image
   }
 
@@ -22,29 +33,13 @@ export class CanvasPageComponent implements OnInit {
     console.log("cleared")
   }
   //Submits the saved drawing to google vision for analysis
-  // submitDrawing(){
-  //   var image = this.saveImage()
-
-  //   const vision = require("@google-cloud/vision")
-
-  //   const client = new vision.ImageAnnotatorClient()
-  //   //   keyFilename: "../../../../droogle-key.json"
-  //   // })
-
-  //   client
-  //     .labelDetection(image)
-  //     .then((results: any) => {
-  //       const labels = results[0].labelAnnotations;
-
-  //       console.log("Labels:");
-  //       labels.forEach((label: any) => console.log(label.description));
-  //     })
-  //     .catch((err: any) => {
-  //       console.error("ERROR:", err)
-  //     })
-  // }
-
-  ngOnInit(): void {
+  submitDrawing(){
+    var image = this.saveImage();
+      this.googlevision.submitDrawingToGoogle(image).then((response) =>
+      {
+        console.log(response);
+        this.googleResponse = response;
+      })
   }
 
 }

2372a23a34e75b43ed3d4ecb99e4fd6636777efd
Khanh Nguyen <knguye15.ccp@gmail.com>
2022-02-07 22:06:27 -0500

    Home page initial styling

--- src/app/services/categoryapi.service.ts
+++ src/app/services/categoryapi.service.ts
@@ -10,14 +10,7 @@ import { Category } from '../models/category';
   providedIn: 'root'
 })
 export class CategoryapiService {
-<<<<<<< HEAD
-  private apiUrl = 'http://webapi-prod.us-west-2.elasticbeanstalk.com/api/category'
   
-
-
-=======
-  
->>>>>>> 4d853f78b5c0a09c1fd1f98a2bf145471ee74ecc
   constructor(private http: HttpClient) { }
 
   private apiUrl = 'http://webapi-prod.us-west-2.elasticbeanstalk.com/api/Category'

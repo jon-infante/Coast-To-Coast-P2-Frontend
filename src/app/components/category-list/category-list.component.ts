9bf75400b94f9ded57c431e1cebcb5e79562db29
Bum Keun Cho <bumkeunc@gmail.com>
2022-02-08 07:34:20 -0800

    Wallpost in Category

--- src/app/components/category-list/category-list.component.ts
+++ src/app/components/category-list/category-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from '../../models/category';
 import { CategoryapiService } from '../../services/categoryapi.service';
+import { Wallpost } from '../../models/wallpost';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'category-list',
@@ -9,7 +11,7 @@ import { CategoryapiService } from '../../services/categoryapi.service';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor(private apiService: CategoryapiService) { }
+  constructor(private apiService: CategoryapiService, private router:Router) { }
 
   allCategories: Category[] = [];
 
@@ -22,9 +24,23 @@ export class CategoryListComponent implements OnInit {
           CategoryName: category.categoryName,
           WallPosts: category.wallPosts,
         }
+        category.wallPosts.forEach((wallpost: any) => {
+          var castedWallPost: Wallpost = {
+            ID: wallpost.id,
+            CategoryID: wallpost.categoryID,
+            Keyword: wallpost.keyword,
+            Drawings: wallpost.drawings
+          }
+          castedCategory.WallPosts.push(castedWallPost)
+        })
         this.allCategories.push(castedCategory)
       })
       console.log(this.allCategories);
     })
+  }
+
+  goToCanvas(id: any): void
+  {
+    this.router.navigate([`canvas/${id}`],);
   }
 }

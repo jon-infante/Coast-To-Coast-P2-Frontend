0ec932cacbdebc2af578c0bf0bc8c47ddf94ddac
jdlr1234 <jdlr123412@gmail.com>
2022-02-10 14:04:56 -0700

    tests for services

--- src/app/services/commentapi.service.spec.ts
+++ src/app/services/commentapi.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing'
 
 import { CommentapiService } from './commentapi.service';
 
@@ -6,11 +7,71 @@ describe('CommentapiService', () => {
   let service: CommentapiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(CommentapiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+    // it('should get comments by drawing is', async () => {
+  //   let fakeData: Comment []=[ 
+  //     {
+  // DrawingID: 1,
+  // UserID: 1,
+  // Username: 'string',
+  // Message: 'string',
+  // Likes: [],
+  // Date: 'string',
+  // IsLiked: true
+  //     },
+    //     {
+  // DrawingID: 2,
+  // UserID: 2,
+  // Username: 'string2',
+  // Message: 'string2',
+  // Likes: [],
+  // Date: 'string2',
+  // IsLiked: true
+  //     },
+  //   ];
+    
+  //   spyOn(service, 'getCommentsByDrawingID').and.returnValue(Promise.resolve(fakeData))
+
+  //   await service.getCommentsByDrawingID().then((res) => {
+  //     //Verify that the response is what we want
+  //     expect(service.getCommentsByDrawingID).toHaveBeenCalled();
+  //     expect(service.getCommentsByDrawingID).toHaveBeenCalledTimes(1);
+  //     expect(res).toBeTruthy();
+  //     expect(res.length).toEqual(2);
+  //   });
+  // });
+    // it('should add comment', async () => {
+  //   let fakeData: Comment = 
+  //     {
+  //
+  // DrawingID: 1,
+  // UserID: 1,
+  // Username: 'string',
+  // Message: 'string',
+  // Likes: [],
+  // Date: 'string',
+  // IsLiked: true
+
+  //     }
+  //   ;
+    
+  //   spyOn(service, 'addComment').and.returnValue(Promise.resolve(fakeData))
+
+  //   await service.addComment().then((res) => {
+  //     //Verify that the response is what we want
+  //     expect(service.addComment).toHaveBeenCalled();
+  //     expect(service.addComment).toHaveBeenCalledTimes(1);
+  //     expect(res).toBeTruthy();
+  //     expect(res.length).toEqual(2);
+  //   });
+  // });
+  
 });
--- src/app/services/drawingapi.service.spec.ts
+++ src/app/services/drawingapi.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing'
 
 import { DrawingapiService } from './drawingapi.service';
 
@@ -6,11 +7,88 @@ describe('DrawingapiService', () => {
   let service: DrawingapiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(DrawingapiService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+        // it('should get all drawing', async () => {
+  //   let fakeData: Drawing []=[ 
+  //     {
+    // ID: 1,
+    // PlayerID: 1,
+    // PlayerName: 'string',
+    // WallPostID: 1,
+    // Keyword: 'string',
+    // BucketImage: 'string',
+    // Guess: true,
+    // GoogleScore: 1,
+    // GoogleResponse: 'string',
+    // Likes: [],
+    // Date: 'string',
+    // IsLiked: true
+
+  //     },
+    //     {
+    // ID: 2,
+    // PlayerID: 2,
+    // PlayerName: 'string2',
+    // WallPostID: 2,
+    // Keyword: 'string2',
+    // BucketImage: 'string2',
+    // Guess: true,
+    // GoogleScore: 2,
+    // GoogleResponse: 'string2',
+    // Likes: [],
+    // Date: 'string2',
+    // IsLiked: true
+
+  //     },
+  //   ];
+    
+  //   spyOn(service, 'getAllDrawings').and.returnValue(Promise.resolve(fakeData))
+
+  //   await service.getAllDrawings().then((res) => {
+  //     //Verify that the response is what we want
+  //     expect(service.getAllDrawings).toHaveBeenCalled();
+  //     expect(service.getAllDrawings).toHaveBeenCalledTimes(1);
+  //     expect(res).toBeTruthy();
+  //     expect(res.length).toEqual(2);
+  //   });
+  // });
+  
+      // it('add drawing to database', async () => {
+  //   let fakeData: Drawing = 
+  //     {
+    // ID: 1,
+    // PlayerID: 1,
+    // PlayerName: 'string',
+    // WallPostID: 1,
+    // Keyword: 'string',
+    // BucketImage: 'string',
+    // Guess: true,
+    // GoogleScore: 1,
+    // GoogleResponse: 'string',
+    // Likes: [],
+    // Date: 'string',
+    // IsLiked: true
+
+  //     }
+  //   ;
+    
+  //   spyOn(service, 'addDrawing').and.returnValue(Promise.resolve(fakeData))
+
+  //   await service.addDrawing().then((res) => {
+  //     //Verify that the response is what we want
+  //     expect(service.addDrawing).toHaveBeenCalled();
+  //     expect(service.addDrawing).toHaveBeenCalledTimes(1);
+  //     expect(res).toBeTruthy();
+  //     expect(res.length).toEqual(2);
+  //   });
+  // });
 });

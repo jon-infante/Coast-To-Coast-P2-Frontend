415c2f6a9a9a3e0f2aa3c71c7f3eb600341636b7
jon-infante <jonjoninfante@gmail.com>
2022-02-10 22:54:50 -0500

    environment url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  droogleAPIRootUrl: 'http://webapi-prod.us-west-2.elasticbeanstalk.com/api',
   authDomain: 'dev-kcqyhpiz.us.auth0.com',
   authClientId: 'QnuIqWwbX6x7KVs42opO2luqJaEu2pXL'
 };
